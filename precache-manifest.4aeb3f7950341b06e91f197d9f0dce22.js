42f329cca3c44d0ec02c97156d384eff1e63cf69
Dawid Glowacki <dawid.glowacki@pl.abb.com>
2019-10-01 21:19:37 +0200

    initial test

---
+++ precache-manifest.4aeb3f7950341b06e91f197d9f0dce22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ec27f0ff6ccc8d7e98b8576f2b27e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a836464c9852954c9c5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "671fdd1435c26f5347ba",
+    "url": "/static/js/2.0d596d35.chunk.js"
+  },
+  {
+    "revision": "7a836464c9852954c9c5",
+    "url": "/static/js/main.ccd4995a.chunk.js"
+  },
+  {
+    "revision": "03b0c23978953aa6bfc8",
+    "url": "/static/js/runtime-main.de7d5539.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
